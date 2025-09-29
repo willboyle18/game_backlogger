877ca6077eb7051165a088b197c8fd41bd2d510e
Will Boyle <willboyle18@gmail.com>
2025-09-29 13:42:31 -0700

    Add composite unique index to prevent duplicates per user + game

---
+++ db/migrate/20250929204116_add_unique_index_to_backlog_items.rb
@@ -0,0 +1,5 @@
+class AddUniqueIndexToBacklogItems < ActiveRecord::Migration[8.0]
+  def change
+    add_index :backlog_items, [:game_id, :user_id], unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_26_185307) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_29_204116) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_26_185307) do
     t.bigint "game_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["game_id", "user_id"], name: "index_backlog_items_on_game_id_and_user_id", unique: true
     t.index ["game_id"], name: "index_backlog_items_on_game_id"
     t.index ["user_id"], name: "index_backlog_items_on_user_id"
   end
