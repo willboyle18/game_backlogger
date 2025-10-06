66ae5ef190fe53d637dc50102b0af19a829e008a
Will Boyle <willboyle18@gmail.com>
2025-10-06 16:59:53 -0700

    Add current_user helper method for views and login_required method to authenticate users in controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   include Authentication
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  helper_method :current_user
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def login_required!
+    redirect_to new_session_path, alert: "Please sign in" unless current_user
+  end
 end
