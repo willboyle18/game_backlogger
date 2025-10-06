40698f4e72cc6244c8282ebe96ed2d508383d8c9
Will Boyle <willboyle18@gmail.com>
2025-10-06 14:03:59 -0700

    Add test user to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create!(
+  email_address: "test@example.com",
+  password: "password",
+  password_confirmation: "password"
+)
