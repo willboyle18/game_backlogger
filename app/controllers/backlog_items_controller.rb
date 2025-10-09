ba88ff8f1d0d678644c387da8923da722a3bfea9
Will Boyle <willboyle18@gmail.com>
2025-10-08 18:20:42 -0700

    Write create and backlog_item_params methods for BacklogItemsController

--- app/controllers/backlog_items_controller.rb
+++ app/controllers/backlog_items_controller.rb
@@ -4,11 +4,23 @@ class BacklogItemsController < ApplicationController
   end
 
   def create
+    @backlog_item = current_user.backlog_items.build(backlog_item_params)
+    if @backlog_item.save
+      redirect_back fallback_location: backlog_items_path, notice: "Added to your backlog"
+    else
+      redirect_back fallback_location: backlog_items_path, alert: "Unable to add to your backlog"
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def backlog_item_params
+    params.require(:backlog_item).permit(:game_id)
   end
 end
