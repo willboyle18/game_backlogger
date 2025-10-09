8eef57832b821eddab4dde87538cd808eb8f2e87
Will Boyle <willboyle18@gmail.com>
2025-10-09 12:26:19 -0700

    Implement basic add and view functionality and fix authentication

--- app/controllers/backlog_items_controller.rb
+++ app/controllers/backlog_items_controller.rb
@@ -1,10 +1,12 @@
 class BacklogItemsController < ApplicationController
+  before_action :require_authentication
   def index
-    @backlog_items = current_user.backlog_items.includes(:game).order(created_at: :desc)
+    @backlog_items = Current.user.backlog_items.includes(:game).order(created_at: :desc)
   end
 
   def create
-    @backlog_item = current_user.backlog_items.build(backlog_item_params)
+    @backlog_item = Current.user.backlog_items.build(backlog_item_params)
+    puts "hello"
     if @backlog_item.save
       redirect_back fallback_location: backlog_items_path, notice: "Added to your backlog"
     else
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,13 +1,30 @@
 class GamesController < ApplicationController
+  before_action :require_authentication
   def index
+    @games = Game.order(created_at: :desc)
   end
 
+  def new
+    @game = Game.new
+  end
   def create
+    @game = Game.new(game_params)
+    if @game.save
+      redirect_to @game, notice: "Game created"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
-  def update
+  def show
+    @game = Game.find(params[:id])
+    @backlog_item = Current.user.backlog_items.find_by(game_id: @game.id) ||
+                    Current.user.backlog_items.build(game: @game)
   end
 
-  def destroy
+  private
+
+  def game_params
+    params.require(:game).permit(:name)
   end
 end
