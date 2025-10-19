eaa1abaa107f79c6c320e54fc23809b3d906a7cb
Will Boyle <willboyle18@gmail.com>
2025-10-18 23:38:32 -0700

    Implement add to backlog feature with IGDB games

--- app/controllers/backlog_items_controller.rb
+++ app/controllers/backlog_items_controller.rb
@@ -5,12 +5,18 @@ class BacklogItemsController < ApplicationController
   end
 
   def create
-    @backlog_item = Current.user.backlog_items.build(backlog_item_params)
-    if @backlog_item.save
-      redirect_back fallback_location: backlog_items_path, notice: "Added to your backlog"
-    else
-      redirect_back fallback_location: backlog_items_path, alert: "Unable to add to your backlog"
+    igdb_id = params.require(:igdb_id).to_i
+    game = Game.find_by(igdb_id:) || insert_game_from_igdb!(igdb_id)
+
+    begin
+      item = Current.user.backlog_items.find_or_create_by!(game: game)
+      flash[:notice] = "Game successfully added to backlog"
+    rescue ActiveRecord::RecordNotUnique
+      # Unique index on [user_id, game_id]
+      flash[:alert] = "Already in backlog"
     end
+
+    redirect_back fallback_location: igdb_search_path(q: params[:q])
   end
 
   def update
@@ -27,5 +33,19 @@ class BacklogItemsController < ApplicationController
 
   def backlog_item_params
     params.require(:backlog_item).permit(:game_id)
+  end
+
+  def insert_game_from_igdb!(igdb_id)
+    Game.transaction(requires_new: true) do
+      Game.find_or_create_by!(igdb_id: igdb_id) do |g|
+        data = IgdbSearch.new.game_by_id(igdb_id) or raise "IGDB not found"
+        g.name = data["name"]
+        g.slug = data["slug"]
+        g.first_release_date = data["first_release_date"]
+        g.cover_image_id = data.dig("cover","image_id")
+        g.rating = data["total_rating"]
+        g.platform_ids = Array(data["platforms"]).map { _1["id"] }
+      end
+    end
   end
 end
--- app/services/igdb_search.rb
+++ app/services/igdb_search.rb
@@ -5,6 +5,16 @@ require "uri"
 class IgdbSearch
   BASE = "https://api.igdb.com/v4".freeze
 
+  DEFAULT_FIELDS = %w[
+    id
+    name
+    slug
+    first_release_date
+    cover.image_id
+    platforms.id
+    total_rating
+  ].freeze
+
   def initialize
     @client_id = ENV["TWITCH_CLIENT_ID"]
     @access_token = ENV["IGDB_ACCESS_TOKEN"]
@@ -21,7 +31,7 @@ class IgdbSearch
     req["Accept"]        = "application/json"
     req["Content-Type"]  = "text/plain"
     req.body = <<~APQ
-      fields name, first_release_date, cover.url, platforms.name, genres.name, slug;
+      fields id, name, first_release_date, cover.url, platforms.name, genres.name, slug;
       search "#{query}";
       limit #{limit};
     APQ
@@ -29,6 +39,29 @@ class IgdbSearch
     Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
       res = http.request(req)
       res.is_a?(Net::HTTPSuccess) ? JSON.parse(res.body) : []
+    end
+  end
+
+  def game_by_id(id, fields: DEFAULT_FIELDS)
+    id = Integer(id)
+    fields_list = Array(fields).join(", ")
+
+    uri = URI("#{BASE}/games")
+    req = Net::HTTP::Post.new(uri)
+    req["Client-ID"]     = @client_id
+    req["Authorization"] = "Bearer #{@access_token}"
+    req["Accept"]        = "application/json"
+    req["Content-Type"]  = "text/plain"
+    req.body = <<~APQ
+      fields #{fields_list};
+      where id = #{id};
+      limit 1;
+    APQ
+
+    Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
+      res = http.request(req)
+      return nil unless res.is_a?(Net::HTTPSuccess)
+      JSON.parse(res.body).first
     end
   end
 end
