9d52f189a8bf81012a5b6a8f466bffe717dac9c9
Will Boyle <willboyle18@gmail.com>
2025-10-18 21:36:40 -0700

    Update Game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,21 @@ class Game < ApplicationRecord
   has_many :users, through: :backlog_items
 
   validates :name, presence: true
+  validates :igdb_id, presence: true, uniqueness: true
+
+  def release_year
+    return nil unless first_release_date
+    ts = first_release_date
+    ts = ts / 1000 if ts > 2_000_000_000
+    Time.at(ts).utc.year
+  end
+
+  def cover_url(size: "t_cover_big")
+    return nil if cover_image_id.blank?
+    "https://images.igdb.com/igdb/image/upload/#{size}/#{cover_image_id}.jpg"
+  end
+
+  def platform_id_list
+    Array(platform_ids).compact
+  end
 end
