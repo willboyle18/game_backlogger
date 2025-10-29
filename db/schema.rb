c0e7423b40b045983adf118b248bacf2e370ff91
Will Boyle <willboyle18@gmail.com>
2025-10-28 18:07:05 -0700

    Add username field to Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_22_192137) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_29_010413) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -66,7 +66,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_22_192137) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "backlog_items", "games"
