e8234559249ba24b5b6a365361f505c93a3d76e3
Will Boyle <willboyle18@gmail.com>
2025-10-30 14:53:53 -0700

    Get status to display on user's backlog

--- app/models/backlog_item.rb
+++ app/models/backlog_item.rb
@@ -2,5 +2,7 @@ class BacklogItem < ApplicationRecord
   belongs_to :user
   belongs_to :game
 
+  enum :status, { not_started: 0, currently_playing: 1, dropped: 2, completed: 3 }
+
   validates :user_id, uniqueness: { scope: :game_id }
 end
